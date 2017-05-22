9980f279751a5d4395705ad3cb293186e6fcf06a
Rajmshah <rajwohlig@gmail.com>
2017-05-22 19:13:44 +0530

    Done changes

--- js/app.js
+++ js/app.js
@@ -1153,3 +1153,19 @@ firstapp.directive('inputDate', function ($compile, $parse) {
         }
     };
 });
+firstapp.directive('onlyAlpha', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                var transformedInput = text.replace(/[^a-zA-Z]/g, '');
+                if (transformedInput !== text) {
+                    ngModelCtrl.$setViewValue(transformedInput);
+                    ngModelCtrl.$render();
+                }
+                return transformedInput; // or return Number(transformedInput)
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
--- js/templateservice.js
+++ js/templateservice.js
@@ -1,5 +1,5 @@
 var templateservicemod = angular.module('templateservicemod', []);
-templateservicemod.service('TemplateService', function() {
+templateservicemod.service('TemplateService', function () {
   this.title = "Home";
   this.meta = "Google";
   this.metadesc = "Home";
@@ -7,8 +7,9 @@ templateservicemod.service('TemplateService', function() {
   this.keywords = "";
   var d = new Date();
   this.year = d.getFullYear();
+  this.onlyAlpha = /^[A-z]+$/;
 
-  this.init = function() {
+  this.init = function () {
     this.headermenu = "views/headermenu.html";
     this.header = "views/header.html";
     this.menu = "views/menu.html";
@@ -18,7 +19,7 @@ templateservicemod.service('TemplateService', function() {
     this.footer = "views/footer.html";
   };
 
-  this.changecontent = function(page) {
+  this.changecontent = function (page) {
     this.init();
     var data = this;
     data.content = "views/content/" + page + ".html";
