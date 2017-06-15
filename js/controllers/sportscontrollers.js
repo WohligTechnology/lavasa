807fb99415425a419878456739fc4c16a05c55c5
Rajmshah <rajwohlig@gmail.com>
2017-06-15 12:36:35 +0530

    removed header

--- js/controllers/sportscontrollers.js
+++ js/controllers/sportscontrollers.js
@@ -1740,10 +1740,6 @@ firstapp.controller('AthletesSelectionCtrl', function ($scope, TemplateService, 
 
     }];
 
-
-
-
-
 })
 
 //Sports-Confirm-Team
@@ -1755,96 +1751,4 @@ firstapp.controller('ConfirmTeamCtrl', function ($scope, TemplateService, Naviga
     TemplateService.header = "views/header2.html";
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
-})
-
-firstapp.controller('headerctrl', function ($scope, TemplateService, $rootScope) {
-    $scope.template = TemplateService;
-    $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-        $(window).scrollTop(0);
-    });
-    $scope.variables = {};
-    $scope.$watch('online', function (newStatus) {
-        $scope.variables.online = $rootScope.online;
-    });
-    $scope.games = // JavaScript Document
-        [{
-            "img": "img/footer/n1.jpg",
-            "href": "http://madeofgreat.tatamotors.com/tiago/",
-            "game": "Fantastico Partner"
-        }, {
-            "img": "img/footer/n2.jpg",
-            "href": "",
-            "game": "Smartphone Partner"
-        }, {
-            "img": "img/footer/p4.jpg",
-            "href": "",
-            "game": "Energy Drinks Partner"
-        }, {
-            "img": "img/footer/n3.jpg",
-            "href": "",
-            "game": "Support Partner"
-        }, {
-            "img": "img/footer/p7.jpg",
-            "href": "",
-            "game": "Media Partner "
-        }, {
-            "img": "img/footer/n4.jpg",
-            "href": "https://www.facebook.com/sportsillustratedindia/",
-            "game": "Magazine Partner"
-        }, {
-            "img": "img/footer/n1.jpg",
-            "href": "http://madeofgreat.tatamotors.com/tiago/",
-            "game": "Fantastico Partner"
-        }, {
-            "img": "img/footer/n2.jpg",
-            "href": "",
-            "game": "Smartphone Partner"
-        }, {
-            "img": "img/footer/p4.jpg",
-            "href": "",
-            "game": "Energy Drinks Partner"
-        }, {
-            "img": "img/footer/n3.jpg",
-            "href": "",
-            "game": "Support Partner"
-        }];
-    $scope.partner = // JavaScript Document
-        [{
-            "img": "img/footer/p1.jpg",
-            "href": "",
-            "game": "Venue Partner"
-        }, {
-            "img": "img/footer/p6.jpg",
-            "href": "",
-            "game": "Hospital Partner"
-        }, {
-            "img": "img/footer/na1.jpg",
-            "href": "",
-            "game": "Sports Equipment Partner"
-        }, {
-            "img": "img/footer/na2.jpg",
-            "href": "",
-            "game": "Apparel Partner"
-        }, {
-            "img": "img/footer/na3.jpg",
-            "href": "",
-            "game": "Sports Surface Partner"
-        }, {
-            "img": "img/footer/na6.jpg",
-            "href": "",
-            "game": "Sports Mentorship Partner"
-        }, {
-            "img": "img/footer/na4.jpg",
-            "href": "",
-            "game": "Shooting Range Partner"
-        }, {
-            "img": "img/footer/p5.jpg",
-            "href": "",
-            "game": "Medical Partner"
-        }, {
-            "img": "img/footer/na5.jpg",
-            "href": "",
-            "game": "Event Partner "
-        }];
-
 })
