c1c989b1757438bf04f667cba4f77b61d6d90e88
ipratikminat <pratik.wohlig@gmail.co>
2017-06-23 17:45:45 +0530

    working on service

--- js/services/selectService.js
+++ js/services/selectService.js
@@ -1,11 +1,61 @@
 firstApp.service('selectService', function ($http, TemplateService, $state) {
 
-    this.team = null;
-    this.editTeam = function (arr, callback) {
-        this.team = _.filter(arr, ['checked', true]);
-        console.log(this.team);
-        callback(this.team);
+    this.team = [];
+    this.sportsId = null;
+    this.gender = null;
+    this.ageGroup = null;
+
+    //make .checked to true if already selected
+    this.isAtheleteSelected = function (listOfAthlete, team) {
+        console.log("aaya");
+        var temp = _.intersectionBy(listOfAthlete, this.team, '_id');
+        _.each(temp, function (n) {
+            n.checked = true;
+        });
+        return listOfAthlete;
+    };
+
+
+    // push to Team
+    this.pushToTeam = function (obj, bool, listOfAthlete) {
+        console.log(obj, bool, listOfAthlete);
+        if (bool) {
+            this.team.push(obj);
+        } else {
+            _.remove(this.team, function (n) {
+                console.log(n._id, obj._id);
+                return n._id == obj._id;
+            });
+            var temp = _.find(listOfAthlete, ['_id', obj._id]);
+            temp.checked = false;
+        }
     }
 
+    this.modifyDataIfInTeam = function (obj) {
+        alert();
+        // check if this is in team 
+        //      if yes status alreadyInTeam
+    };
+
+
+    this.getAgeGroupByAthelete = function (athelete, sports) {
+        // athlete birthdate
+        // _.filter(sports,between athlete birthdate and filter by gender as well);
+    }
+
+    this.getSportFromFilters = function (filter, sports) {
+        // filter to find sports list for all
+    }
+
+    this.formatDataForSending = function () {
+        // _.map();
+    }
+
+    this.reset = function () {
+        this.team = [];
+        this.sportsId = null;
+        this.gender = null;
+        this.ageGroup = null;
+    }
 
 });
