789b509dfebe117de544e0cd273290d9b9eb6d40
Rajmshah <rajwohlig@gmail.com>
2018-10-26 12:56:51 +0530

    done live update settings

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -4603,14 +4603,14 @@ firstApp.controller('headerctrl', function ($scope, TemplateService, $rootScope,
                     if (type == 'school') {
                         NavigationService.setSfaCity(selectedCity);
                         $scope.sfaCity = 'hyderabad';
-                        sublink = sublink3 + "/draws-schedule";
-                        // sublink = "https://" + link2 + "/liveupdates";
+                        // sublink = sublink3 + "/draws-schedule";
+                        sublink = "https://" + link2 + "/liveupdates";
                         window.open(sublink, '_self');
                     } else if (type == 'college') {
                         NavigationService.setSfaCity(selectedCity);
                         $scope.sfaCity = 'hyderabad';
-                        sublink = sublink4 + "/draws-schedule";
-                        // sublink = sublink4 + "/liveupdates";
+                        // sublink = sublink4 + "/draws-schedule";
+                        sublink = sublink4 + "/liveupdates";
                         window.open(sublink, '_self');
                     }
                     break;
@@ -4619,14 +4619,14 @@ firstApp.controller('headerctrl', function ($scope, TemplateService, $rootScope,
                     if (type == 'school') {
                         NavigationService.setSfaCity(selectedCity);
                         $scope.sfaCity = 'hyderabad';
-                        sublink = sublink3 + "/draws-schedule";
-                        // sublink = "https://" + link2 + "/liveupdates";
+                        // sublink = sublink3 + "/draws-schedule";
+                        sublink = "https://" + link2 + "/liveupdates";
                         window.open(sublink, '_self');
                     } else if (type == 'college') {
                         NavigationService.setSfaCity(selectedCity);
                         $scope.sfaCity = 'hyderabad';
-                        sublink = sublink4 + "/draws-schedule";
-                        // sublink = sublink4 + "/liveupdates";
+                        // sublink = sublink4 + "/draws-schedule";
+                        sublink = sublink4 + "/liveupdates";
                         window.open(sublink, '_self');
                     }
                     break;
@@ -4634,14 +4634,14 @@ firstApp.controller('headerctrl', function ($scope, TemplateService, $rootScope,
                     if (type == 'school') {
                         NavigationService.setSfaCity(selectedCity);
                         $scope.sfaCity = 'hyderabad';
-                        sublink = sublink3 + "/draws-schedule";
-                        // sublink = "https://" + link2 + "/liveupdates";
+                        // sublink = sublink3 + "/draws-schedule";
+                        sublink = "https://" + link2 + "/liveupdates";
                         window.open(sublink, '_self');
                     } else if (type == 'college') {
                         NavigationService.setSfaCity(selectedCity);
                         $scope.sfaCity = 'hyderabad';
-                        sublink = sublink4 + "/draws-schedule";
-                        // sublink = sublink4 + "/liveupdates";
+                        // sublink = sublink4 + "/draws-schedule";
+                        sublink = sublink4 + "/liveupdates";
                         window.open(sublink, '_self');
                     }
                     break;
@@ -4923,7 +4923,7 @@ firstApp.controller('headerctrl', function ($scope, TemplateService, $rootScope,
     } else if (window.location.host == link3) {
         $scope.selectedCity = 'ahmedabad';
     }
-  
+
     var a = window.location.host;
     $scope.infoEmail = [];
 
@@ -5001,7 +5001,7 @@ firstApp.controller('footerctrl', function ($scope, TemplateService, $rootScope,
     } else if (window.location.host == link3) {
         $scope.selectedCity = 'ahmedabad';
     }
-  
+
     $scope.getSponsor = function () {
         if (window.location.host == link1) {
             $scope.url = sublink1 + '/api/SponsorPage/getAllBySponsorType';
